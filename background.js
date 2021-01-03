b7e94009dea7e2dd871313d39a4d81f88582485b
missoum1307 <missoumozil@gmail.com>
2021-01-03 22:09:16 +0100

    fixing [translating not showing sometimes bug

--- background.js
+++ background.js
@@ -8,7 +8,8 @@ document.body.appendChild(d1o3)
 //and cross origin requests are blocked
 /* 
 var RemoveCSPAndAddOrigin = function(details) {
-    for (var i = 0; i < details.responseHeaders.length; i++) {
+    for (var i = 0;
+     i < details.responseHeaders.length; i++) {
         if (details.responseHeaders[i].name.toLowerCase() === 'content-security-policy') {
             details.responseHeaders[i].value = ''
         }
@@ -26,41 +27,71 @@ var extraInfo = ['blocking', 'responseHeaders']
 chrome.webRequest.onHeadersReceived.addListener(RemoveCSPAndAddOrigin, networkFilters, extraInfo)
 */
 
+
+let ArrayOfTranslations = []
+
 chrome.runtime.onMessage.addListener(
   (request, sender, sendResponse) => { 
+ 
+
 	// Get the word definition and pronunciation from oxfordlearnersdictionaries.com
 	// and translation from reverso.net
-	
   	(async function fetchDefinition() {
-  		
-  		if (request.message.endsWith('\'s'))  request.message = request.message.slice(0, -2)
-  		var res = await fetch('https://www.oxfordlearnersdictionaries.com/search/english/?q=' + request.message)
-  		document.getElementById("DefinitioRender").innerHTML = await res.text()
+      async function show(){
+        let data = JSON.parse(oReq.response)
+        if (data.contextResults.results.length == 0) {
+          ArrayOfTranslations.push(data.translation)
+        } else {
+          for (var i = 0; i < data.contextResults.results.length; i++) {
+            ArrayOfTranslations.push(data.contextResults.results[i].translation)
+          }
+        }
 
-  		if(document.getElementsByClassName('def').length > 0) {
+        var res = await fetch('https://www.oxfordlearnersdictionaries.com/search/english/?q=' + request.message)
+        document.getElementById("DefinitioRender").innerHTML = await res.text()
+        if(document.getElementsByClassName('def').length > 0) {
+          var DefinitionString = document.getElementsByClassName('def')[0].innerText
+          var pronunciationLink = document.body.getElementsByClassName('sound')[1].dataset.srcMp3
+          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {message: [DefinitionString, pronunciationLink, ArrayOfTranslations] }, function(response) {
 
-  			var DefinitionString = document.getElementsByClassName('def')[0].innerText
-  			var pronunciationLink = document.body.getElementsByClassName('sound')[1].dataset.srcMp3
+            })
+          })
+        } else { 
+          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {message: '' }, function(response) {
 
-  			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, {message: [DefinitionString, pronunciationLink] }, function(response) {
-    		 //console.log(response.message)
-  			})
-			})  			
-  		} else { 
-  			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, {message: '' }, function(response) {
-    		// console.log(response.message)
-  				})
-			})
-  		}
+            })
+          })
+        }
+      } 
 
-	})()
+      ArrayOfTranslations = []
 
+      if (request.message.endsWith('\'s'))  {
+        request.message = request.message.slice(0, -2)
+      }
 
-
-	
-
+      var oReq = new XMLHttpRequest()
+      oReq.addEventListener("load", () => {
+        show()
+      })
+      oReq.open("POST", "https://api.reverso.net/translate/v1/translation", true)
+      oReq.setRequestHeader('Content-Type', 'application/json')
+      oReq.send(JSON.stringify({
+        "input":request.message,
+        "from":"eng",
+        "to":"ara",
+        "format":"text",
+        "options":{
+        "origin":"reversodesktop",    
+        "sentenceSplitter":false,
+        "contextResults":true, 
+        "languageDetection":false
+        }}))
+    })()
 })
+
+
 
 // fix the 's ending words ex:  browser's wont return no result
--- contentScript.js
+++ contentScript.js
@@ -6,8 +6,6 @@ var removeWhiteSpaces = function(a) {
 
 // variable holding the value of the clicked/selected word      
 let wordClicked = window.getSelection()
-let ArrayOfTranslations = []
-// let ArrayOfWordPosition = []
 
 // inserting the pop up box (container) of the definition for later use of showing
 // the definition in it.
@@ -22,20 +20,6 @@ document.body.append(d1o1)
 document.getElementById('DefinitionPopup').innerHTML = BoxTextElement
 
 
-async function fetchTransition(url, PostDataObject) {
-  		var response = await fetch(url, {
-    		method: 'POST',
-    		headers: {
-      			'Content-Type': 'application/json'
-    		},
-    	    body: JSON.stringify(PostDataObject),
-    	    referrerPolicy: 'no-referrer'
-  		})
-
-  		return response.json()
-	
-}
-
 window.addEventListener('dblclick', (e) => {
 
 	//oRange = wordClicked.getRangeAt(0)
@@ -43,64 +27,33 @@ window.addEventListener('dblclick', (e) => {
 	//console.log(oRange)
 	//console.log(wordClicked)
 
-	// if (e.pageY > window.innerHeight/ 2 ) e.pageY = e.pageY - 300
-	// if (e.pageX > window.innerWidth/ 2 ) e.pageX = e.pageX - 300	
-	if (e.pageY > window.innerHeight/ 2 && e.pageX > window.innerWidth/ 2) {
-		document.getElementById('DefinitionPopup').setAttribute('style', 'margin-left: -190px; margin-top: -190px; top: ' + e.pageY + 'px; left: ' + e.pageX + 'px; border-radius: 7px; background: rgb(255, 255, 0); width: 15%; padding: 5px; position: absolute; z-index: 2147483647; overflow-wrap: break-word;')
-	} else if (e.pageY > window.innerHeight/ 2) {
-			 	document.getElementById('DefinitionPopup').setAttribute('style', 'margin-top: -190px; top: ' + e.pageY + 'px; left: ' + e.pageX + 'px; border-radius: 7px; background: rgb(255, 255, 0); width: 15%; padding: 5px; position: absolute; z-index: 2147483647; overflow-wrap: break-word;')
-	} else if ( e.pageX > window.innerWidth/ 2 ) {
-					 	document.getElementById('DefinitionPopup').setAttribute('style', 'margin-left: -190px; top: ' + e.pageY + 'px; left: ' + e.pageX + 'px; border-radius: 7px; background: rgb(255, 255, 0); width: 15%; padding: 5px; position: absolute; z-index: 2147483647; overflow-wrap: break-word;')
-	} else { 
-
-		document.getElementById('DefinitionPopup').setAttribute('style', 'top: ' + e.pageY + 'px; left: ' + e.pageX + 'px; border-radius: 7px; background: rgb(255, 255, 0); width: 15%; padding: 5px; position: absolute; z-index: 2147483647; overflow-wrap: break-word;')
-
+	if (e.pageY < window.innerHeight/ 2 && e.pageX < window.innerWidth/ 2) {
+		document.getElementById('DefinitionPopup').setAttribute('style', 'display: none; top: ' + e.pageY + 'px; left: ' + e.pageX + 'px; border-radius: 7px; background: rgb(255, 255, 0); width: 15%; padding: 5px; position: absolute; z-index: 2147483647; overflow-wrap: break-word;')
 	}
-	
-	 	// e.pageY = e.pageY + 'px; margin-top: -15%;'
-	 	// document.getElementById('DefinitionPopup').setAttribute('style', 'margin-top: -15%')
+
+	if (e.pageY < window.innerHeight/ 2 && e.pageX > window.innerWidth/ 2) {
+		document.getElementById('DefinitionPopup').setAttribute('style', 'margin-left: -190px; display: none; top: ' + e.pageY + 'px; left: ' + e.pageX + 'px; border-radius: 7px; background: rgb(255, 255, 0); width: 15%; padding: 5px; position: absolute; z-index: 2147483647; overflow-wrap: break-word;')
+	}
+
+	if (e.pageY > window.innerHeight/ 2 && e.pageX < window.innerWidth/ 2) {
+		document.getElementById('DefinitionPopup').setAttribute('style', 'margin-top: -190px; display: none; top: ' + e.pageY + 'px; left: ' + e.pageX + 'px; border-radius: 7px; background: rgb(255, 255, 0); width: 15%; padding: 5px; position: absolute; z-index: 2147483647; overflow-wrap: break-word;')
+	}
+
+	if (e.pageY > window.innerHeight/ 2 && e.pageX > window.innerWidth/ 2) {
+		document.getElementById('DefinitionPopup').setAttribute('style', 'margin-left: -190px; margin-top: -190px; display: none; top: ' + e.pageY + 'px; left: ' + e.pageX + 'px; border-radius: 7px; background: rgb(255, 255, 0); width: 15%; padding: 5px; position: absolute; z-index: 2147483647; overflow-wrap: break-word;')
+	}
 
 
-	//document.getElementById('DefinitionPopup').setAttribute('style', 'top: ' + e.pageY + ' left: ' + e.pageX + 'px; border-radius: 7px; background: rgb(255, 255, 0); width: 15%; padding: 5px; position: absolute; z-index: 2147483647; overflow-wrap: break-word;')
-	
-
-	var PostDataObject = {
-	"input":removeWhiteSpaces(wordClicked.toString().toLowerCase()).endsWith('\'s') ? wordClicked.toString().toLowerCase().slice(0, -2) : removeWhiteSpaces(wordClicked.toString().toLowerCase()),
-	"from":"eng",
-	"to":"ara",
-	"format":"text",
-	"options":{
-		"origin":"reversodesktop",		"sentenceSplitter":false,
-		"contextResults":true, 
-		"languageDetection":false
-	}}
-		
 	if (typeof wordClicked.toString().toLowerCase() === 'string' && removeWhiteSpaces(wordClicked.toString().toLowerCase()) !== '') {
 
 			chrome.runtime.sendMessage({message: removeWhiteSpaces(wordClicked.toString().toLowerCase())}, (response) => {
 			})
-
-			// ArrayOfWordPosition.push(e.pageY, e.pageX)
-
-			fetchTransition('https://api.reverso.net/translate/v1/translation', PostDataObject)
-  			.then((data) => {
-
-  				if (data.contextResults.results.length == 0) {
-  					ArrayOfTranslations.push(data.translation)
-  				} else {
-  					for (var i = 0; i < data.contextResults.results.length; i++) {
-
-  		   	 		ArrayOfTranslations.push(data.contextResults.results[i].translation)
-  		   			}
-  				}
-
-  		   	})
-
 	} 
 })
 
 // awaiting for the definition translation and pronunciation to come from 
 // background.js and then show them to the user. 
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => { 
 
 	if (request.message != '') {
@@ -108,17 +61,17 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 			document.getElementById('playPronc').src = request.message[1]
 			document.getElementById('selectedWord').childNodes[0].textContent = removeWhiteSpaces(wordClicked.toString().toLowerCase()).endsWith('\'s') ? wordClicked.toString().toLowerCase().slice(0, -2) : wordClicked
 			document.getElementById('englishDefinition').textContent = request.message[0]
-			document.getElementById('foreignTranslation').textContent = ArrayOfTranslations
+			document.getElementById('foreignTranslation').textContent = request.message[2]
 			document.getElementById('selectedWord').style.display = ''
 			document.getElementById('DefinitionPopup').style.display = ''
 
 			ArrayOfTranslations = []
 
 	}
-
 	 
 })
 
+// hiding the pop up box (container) of the definition after reading it. 
 document.addEventListener('keydown', function (event) {
   if (event.key === 'Escape') {
     document.getElementById('DefinitionPopup').style.display = 'none'
@@ -133,10 +86,12 @@ window.addEventListener('click', (e) => {
 		document.getElementById('playPronc').play()
 	} else if (!e.target.parentElement || e.target.parentElement.id !== 'DefinitionPopup' && e.target.id !== 'playProncButton') {
 		document.getElementById('DefinitionPopup').style.display = 'none'
-		ArrayOfWordPosition = []
+		document.getElementById('playPronc').src = ''
+		document.getElementById('selectedWord').childNodes[0].textContent = ''
+		document.getElementById('englishDefinition').textContent = ''
+		document.getElementById('foreignTranslation').textContent = ''
 		ArrayOfTranslations = []
 	} 
 })
 
 
-// fixing tran not showing when there is only one tran  margin-top: -7%; margin-left: -7%; 
